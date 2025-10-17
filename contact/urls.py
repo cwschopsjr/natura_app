fd068043fca4bd78a679347d69c9a6230f1433d2
cwschopsjr <cwschops.jr@gmail.com>
2025-10-17 10:17:32 -0300

    Adicionei a opção de editar o produto - url e grupo de usuario

--- contact/urls.py
+++ contact/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     # contact (CRUD)
     path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('user/login/', views.login_view, name='login'),
     path('user/logout/', views.logout_view, name='logout'),
     path('user/create/', views.register, name='register'),
