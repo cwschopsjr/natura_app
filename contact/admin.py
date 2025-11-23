b27fd621f3c1138d1a9da80e0199dac12602a60f
cwschopsjr <cwschops.jr@gmail.com>
2025-11-23 16:37:19 -0300

    Ajustes no admin

--- contact/admin.py
+++ contact/admin.py
@@ -23,20 +23,20 @@ class ContactAdmin(admin.ModelAdmin):
     
 @admin.register(models.Entradas)
 class EntradasAdmin(admin.ModelAdmin):
-    list_display = 'data_de_entrada', 'descricao_do_produto', 'qtd', 'preco_de_custo', 'data_de_validade', 'show'
+    list_display = 'data_de_entrada', 'descricao_do_produto__descricao_do_produto', 'qtd', 'preco_de_custo', 'data_de_validade', 'show'
     ordering = '-id',
-    search_fields = 'id', 'descricao_do_produto', 'qtd', 'preco_de_custo'
+    search_fields = 'data_de_entrada', 'id', 'descricao_do_produto__descricao_do_produto', 'qtd', 'preco_de_custo'
     list_per_page = 300
     list_max_show_all = 300
     list_editable = 'show',
-    list_display_links = 'descricao_do_produto',
+    list_display_links = 'descricao_do_produto__descricao_do_produto',
     
 @admin.register(models.Saidas)
 class SaidasAdmin(admin.ModelAdmin):
-    list_display = 'data_de_saida', 'descricao_do_produto', 'qtd', 'preco_de_venda', 'forma_de_pagamento', 'cliente', 'show'
+    list_display = 'data_de_saida', 'descricao_do_produto__descricao_do_produto', 'qtd', 'preco_de_venda', 'forma_de_pagamento', 'cliente', 'show'
     ordering = '-id',
-    search_fields = 'id', 'descricao_do_produto', 'qtd', 'preco_de_venda'
+    search_fields = 'data_de_saida', 'id', 'descricao_do_produto__descricao_do_produto', 'qtd', 'preco_de_venda'
     list_per_page = 300
     list_max_show_all = 300
     list_editable = 'show',
-    list_display_links = 'descricao_do_produto',
+    list_display_links = 'descricao_do_produto__descricao_do_produto',
